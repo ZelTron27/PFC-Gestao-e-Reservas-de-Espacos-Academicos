f71f105cfa8e7d61d7ead348eb57c9898306b0fb
D3k3 <matheusms2013@gmail.com>
2026-09-03 20:27:34 -0300

    Atualização na entidade Room.java

--- src/main/java/br/com/classholder/classholder/room/domain/Room.java
+++ src/main/java/br/com/classholder/classholder/room/domain/Room.java
@@ -1,10 +1,9 @@
 package br.com.classholder.classholder.room.domain;
 
-import jakarta.persistence.Entity;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.Table;
+import jakarta.persistence.*;
+import jakarta.validation.constraints.Min;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
@@ -24,4 +23,24 @@ public class Room {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    // Nome ou identificação da sala (Ex: Sala 1002, 01-T2, etc)
+    @NotBlank(message = "Nome (ou identificação) da sala é obrigatório")
+    @Column(nullable = false, length = 150)
+    private String roomName;
+
+    //Tipo de sala (Ex: Laboratório de química, Eletrônica, Informática, etc)
+    @NotBlank(message = "Tipo de sala é obrigatório")
+    @Column(nullable = false, length = 250)
+    private String roomType;
+
+    //Localização da sala (Ex: Prédio 1, Corredor de design, etc)
+    @NotBlank(message = "Localização da sala é obrigatório")
+    @Column(nullable = false, length = 350)
+    private String localization;
+
+    //Número máximo de cabeças dentro de uma sala de aula (contando geralmente apenas os alunos)
+    @NotNull(message = "O número máximo de pessoas é obrigatório") // NotNull no lugar de NotBlank pq o segundo serve mais para string.
+    @Min(value = 1, message = "A capacidade da sala não pode ser menor que 1")
+    @Column(nullable = false)
+    private Integer roomCapacity;
 }
