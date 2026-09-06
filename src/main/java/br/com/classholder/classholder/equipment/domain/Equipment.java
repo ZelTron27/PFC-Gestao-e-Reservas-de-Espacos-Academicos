fb08a48f4b5363b83a6eff0adfdb3f6b2a2499c2
D3k3 <matheusms2013@gmail.com>
2026-09-06 18:49:36 -0300

    Atualização nas entidades.

---
+++ src/main/java/br/com/classholder/classholder/equipment/domain/Equipment.java
@@ -0,0 +1,39 @@
+package br.com.classholder.classholder.equipment.domain;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import jakarta.validation.constraints.NotBlank;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Entity
+@Table(name = "equipamentos")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class Equipment {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    // Nome do equipamento (Ex: Projetor, Computador, Lousa digital, etc)
+    // Unique no column serve pra não ter 300 itens com o nome "Projetor" por exemplo.
+    @NotBlank(message = "Nome do equipamento é obrigatório")
+    @Column(nullable = false, unique = true, length = 150)
+    private String name;
+
+    // Modelo ou especificação do equipamento (Ex: Epson PowerLite X, Dell OptiPlex) em casos de equipamentos genéricos, apenas específicar genérico basta.
+    @NotBlank(message = "O modelo do equipamento é obrigatório")
+    @Column(length = 150)
+    private String model;
+}
